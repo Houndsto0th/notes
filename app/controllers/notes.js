6010b387b24b4b4f84b2ce35b5b60ccdbb15b4a1
Houndsto0th <bry.kinnear@gmail.com>
2015-01-30 11:27:40 -0700

    added component for notes

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -11,9 +11,9 @@ export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
       var body = this.get('noteCopy');
-
+      var title = this.get('noteTitle');
       if (body) {
-      var note = this.store.createRecord('note', { body: body });
+      var note = this.store.createRecord('note', { body: body, title: title});
       this.set('noteCopy', '');
       note.save();
       this.set('isAddingNewNote', false);
